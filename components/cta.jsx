a4cc3d074af07ffddda6876dda8447fc9a1c5dc3
Leonardo Diniz <137980570+lpndev@users.noreply.github.com>
2024-05-04 11:48:44 -0700

    style: fix styling errors

--- components/cta.jsx
+++ components/cta.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-import { platforms, upworkUser } from '@/lib/data'
+import { platforms } from '@/lib/data'
 
 import Container from '@/components/container'
 import Section from '@/components/section'
@@ -9,7 +9,7 @@ import Section from '@/components/section'
 function CallToAction() {
   return (
     <Section>
-      <Container className='border-foreground-800 text-foreground-900 gap-8 rounded-lg border bg-primary-foreground/10 px-4 py-8'>
+      <Container className='gap-8 rounded-lg border border-primary bg-primary px-4 py-8 text-primary-foreground'>
         <div className='flex flex-col items-center gap-2'>
           <span className='opacity-60'>Hire me</span>
           <h3 className='scroll-m-20 text-center text-2xl font-semibold tracking-tight first:mt-0'>
@@ -22,7 +22,7 @@ function CallToAction() {
               key={i}
               href={platform.hash}
               target='_blank'
-              className='border-foreground-200 inline-flex w-full items-center gap-2 rounded-lg border bg-primary-foreground p-4 transition hover:brightness-[103%] sm:w-fit'
+              className='inline-flex w-full items-center gap-2 rounded-lg border border-primary-foreground/5 bg-primary-foreground/5 p-4 transition hover:brightness-[103%] sm:w-fit'
             >
               <Image
                 src={platform.icon}
